34fadf12570d5fc00607b36580f6054b69e04c8f
PauloPereira88 <paulo.kemilly@gmail.com>
2026-04-23 08:24:45 -0400

    menu finalizado

---
+++ script.js
@@ -0,0 +1,23 @@
+const btnMenu = document.querySelector('#header nav button');
+
+btnMenu.addEventListener('click', function() {
+    const menuExpansivo = document.querySelector('#header nav ul');
+
+    // //Transforma o DOMTOKENlist em um Arrey
+    // const classes = Array.from(menuExpansivo.classList);
+
+    // //se Existir o valor "Ativado" dentro do Array de classes
+    // if (classes.includes("ativado")) {
+
+    //     //Remove a classes da lista
+    //     menuExpansivo.classList.remove("ativado");
+
+    // } else {
+
+    //     //Adiciona a classes da lista
+    //     menuExpansivo.classList.add("ativado");
+
+    // }
+
+    menuExpansivo.classList.toggle("Ativado");
+});
